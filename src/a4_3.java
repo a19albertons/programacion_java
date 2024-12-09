9f4763a8ddee35cddc1d94c48d421e2f8e6804fd
a19albertons <a19albertons@iessanclemente.net>
2024-12-09 14:26:18 +0100

    03/12/2024 Boletin Java Ejercicios de arrays multidimensionales de acepta el reto. 3 compatible con acepta el reto se usa un array de donde sacaremos los caracteres a la hora de la salida

--- src/a4_3.java
+++ src/a4_3.java
@@ -8,25 +8,35 @@ public class a4_3 {
         sc.nextLine();
         while (filas!= 0 && columnas!=0){
 //        Creacion tablero
-            String[] tablero= new String[filas];
+            char[][] tablero= new char[filas][columnas];
             for (int i=0;i<filas;i++){
-                tablero[i]=sc.nextLine();
+                tablero[i]=sc.nextLine().toCharArray();
             }
 
-//        Gestion daltonicos
-            int num_daltonicos=sc.nextInt();
-            sc.nextLine();
-            for (int i=0;i<num_daltonicos;i++){
-                char viejo=sc.next().charAt(0);
-                char nuevo=sc.next().charAt(0);
-                for (int j=0;j<filas;j++){
-                    tablero[j]=tablero[j].replace(viejo,nuevo);
+
+//        Gestion daltonicos mejorada para aceptar el reto profesor
+            char[] correspondenciaDaltonica =new char[127];
+            for (char c='A'; c<='Z';c++){
+                correspondenciaDaltonica[c]=c;
+            }
+//            procesar copistas
+            int numCopista=sc.nextInt();
+            for (int i=0;i<numCopista;i++){
+                char colorOrig=sc.next().charAt(0);
+                char colorNuevo=sc.next().charAt(0);
+                for (char c='A'; c<='Z';c++){
+                    if (correspondenciaDaltonica[c]==colorOrig){
+                        correspondenciaDaltonica[c]=colorNuevo;
+                    }
                 }
             }
 
 //        Salida
             for (int i=0;i<filas;i++){
-                System.out.println(tablero[i]);
+                for (int j = 0; j < columnas; j++) {
+                    System.out.print(correspondenciaDaltonica[tablero[i][j]]);
+                }
+                System.out.println();
             }
             filas= sc.nextInt();
             columnas=sc.nextInt();
